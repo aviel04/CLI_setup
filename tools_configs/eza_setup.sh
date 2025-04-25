71c08a963272991ec912a7d8c69a890653755d1a
root <root@DESKTOP-7HK1EIC>
2025-04-25 13:06:29 +0300

    update eza_setup.sh

--- tools_configs/eza_setup.sh
+++ tools_configs/eza_setup.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
+# -------- make sure gpg is installed -------- #
+sudo apt update
+sudo apt install -y gpg
 
+# ---------- Install Eza ---------- #
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
 # eza configuration
 #
 #start of Cat
 cat <<EOF >> ~/.bashrc
-	alias ls='eza --color=always --group-directories-first --icons'
-	alias ll='eza -la --icons --octal-permissions --group-directories-first'
-	alias l='eza -bGF --header --git --color=always --group-directories-first --icons'
-	alias llm='eza -lbGd --header --git --sort=modified --color=always --group-directories-first --icons' 
-	alias la='eza --long --all --group --group-directories-first'
-	alias lx='eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale --color=always --group-directories-first --icons'
+alias ls='eza --color=always --group-directories-first --icons'
+alias ll='eza -la --icons --octal-permissions --group-directories-first'
+alias l='eza -bGF --header --git --color=always --group-directories-first --icons'
+alias llm='eza -lbGd --header --git --sort=modified --color=always --group-directories-first --icons' 
+alias la='eza --long --all --group --group-directories-first'
+alias lx='eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale --color=always --group-directories-first --icons'
 
-	alias lS='eza -1 --color=always --group-directories-first --icons'
-	alias lt='eza --tree --level=2 --color=always --group-directories-first --icons'
-	alias l.="eza -a | grep -E '^\.'"
-	EOF
+alias lS='eza -1 --color=always --group-directories-first --icons'
+alias lt='eza --tree --level=2 --color=always --group-directories-first --icons'
+alias l.="eza -a | grep -E '^\.'"
+EOF
 
 # end of cat
